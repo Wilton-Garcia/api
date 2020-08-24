65bd3c909c1f04bb44079ad1734dbef29384590b
Wilton Garcia <wiltongarciaweb@gmail.com>
2020-08-24 20:56:05 -0300

    Add Programming languages

--- mydata/personal_data.py
+++ mydata/personal_data.py
@@ -12,6 +12,29 @@ DATA = [
     }
 ]
 
+LANGUAGES = [
+    {
+        "Linguagem":"CSharp",
+        "Proeficiencia":"70%" 
+    },
+      {
+        "Linguagem":"Java",
+        "Proeficiencia":"60%" 
+    },
+      {
+        "Linguagem":"Java Script",
+        "Proeficiencia":"50%" 
+    },
+      {
+        "Linguagem":"Python",
+        "Proeficiencia":"35%" 
+    },
+]
+
 @app.get("/info")
 def info():
     return DATA
+
+@app.get("/languages")
+def languages():
+    return LANGUAGES
